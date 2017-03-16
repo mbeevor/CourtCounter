900666953467361842cec1cad7f1deb512bf6ffe
mbeevor <mbeevor@gmail.com>
2017-03-16 21:09:11 +0000

    Setting up the methods - Hello Team B!

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -6,10 +6,12 @@ import android.view.View;
 import android.widget.TextView;
 
 import static com.example.android.courtcounter.R.id.team_a_score;
+import static com.example.android.courtcounter.R.id.team_b_score;
 
 public class MainActivity extends AppCompatActivity {
 
     int scoreTeamA = 0;
+    int scoreTeamB = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
     /**
      *  This method is called when the +3 button is clicked.
      */
-    public void threepoints(View view) {
+    public void TeamAThreePoints(View view) {
         scoreTeamA = scoreTeamA + 3;
         displayForTeamA (scoreTeamA);
     }
@@ -35,7 +37,7 @@ public class MainActivity extends AppCompatActivity {
     /**
      *  This method is called when the +2 button is clicked.
      */
-    public void twopoints(View view) {
+    public void TeamATwoPoints(View view) {
         scoreTeamA = scoreTeamA + 2;
         displayForTeamA (scoreTeamA);
     }
@@ -43,10 +45,41 @@ public class MainActivity extends AppCompatActivity {
     /**
      *  This method is called when the +1 button is clicked.
      */
-    public void onepoint(View view) {
+    public void TeamAOnePoint(View view) {
         scoreTeamA = scoreTeamA + 1;
         displayForTeamA (scoreTeamA);
     }
 
 
+    /**
+     * Displays the given score for Team B.
+     */
+    public void displayForTeamB(int score) {
+        TextView scoreView = (TextView) findViewById(team_b_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    /**
+     *  This method is called when the +3 button is clicked.
+     */
+    public void TeamBThreePoints(View view) {
+        scoreTeamB = scoreTeamB + 3;
+        displayForTeamB (scoreTeamB);
+    }
+
+    /**
+     *  This method is called when the +2 button is clicked.
+     */
+    public void TeamBTwoPoints(View view) {
+        scoreTeamB = scoreTeamB + 2;
+        displayForTeamB (scoreTeamB);
+    }
+
+    /**
+     *  This method is called when the +1 button is clicked.
+     */
+    public void TeamBOnePoint(View view) {
+        scoreTeamB = scoreTeamB + 1;
+        displayForTeamB (scoreTeamB);
+    }
 }
